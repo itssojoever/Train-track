482137b96e2dac9c2bd197c05f8e8096e919f35c
JoeEnterAUsername <144183809+JoeEnterAUsername@users.noreply.github.com>
2024-08-01 19:23:06 +0100

    Commit blatant misunderstanding of how classes work

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #A program to receive and display UK train service information
+#Schedule to email train times every morning (after classes too?)
 
 import os
 
@@ -8,28 +9,44 @@ from datetime import datetime
 
 load_dotenv()
 
+arrivalSwitch = 2 #1 for arrival time, #2 for departure time
 maxServices = 2 #Maximum number of services to display
+platform = 1 #Which platform to check
+
 apiKey = os.getenv("apiKey")
 CRS = os.getenv("CRS").upper().strip()
 
-def getServiceData():
+class trainFetcher():
 
-    Darwin = DarwinLdbSession(wsdl="https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx", api_key=apiKey)
+    def serviceDisruptionCheck():
+        pass
 
-    board = Darwin.get_station_board(crs=CRS)
+    def getServiceData(arrivalSwitch, maxServices):
 
-    for service in board.train_services:
-        if maxServices <=0:
-            break
-        
-        destination = service.destination_text
-        print (f"Destination : {destination}")
-        dueStatus = service.etd
-        print (f"Departure time : {dueStatus}")
-        operator = service.operator_name
-        print (f"TOC : {operator}")
-        platform = service.platform
-        print (f"Platform : {platform}")
-        print ("------------------------------------------")
+        Darwin = DarwinLdbSession(wsdl="https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx", api_key=apiKey)
 
-        maxServices -= 1
+        board = Darwin.get_station_board(crs=CRS)
+
+        services = Darwin.get_station_board(crs=CRS)
+
+        for service in board.train_services:
+            if maxServices <=0:
+                break
+            
+            destination = service.destination_text
+            print (f"Destination : {destination}")
+            if arrivalSwitch == 2:
+                departureTime = service.std
+                print (f"Departure time : {departureTime}")
+            elif arrivalSwitch == 1:
+                arrivalTime = service.sta
+                print (f"Arrival time: {arrivalTime}")
+            operator = service.operator_name
+            print (f"TOC : {operator}")
+            platform = service.platform
+            print (f"Platform : {platform}")
+            print ("------------------------------------------")
+
+            maxServices -= 1
+
+trainFetcher.getServiceData(arrivalSwitch, maxServices)
