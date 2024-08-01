0a62ea6bb24dca97839b88d5a474e82aeb387116
JoeEnterAUsername <144183809+JoeEnterAUsername@users.noreply.github.com>
2024-08-01 18:14:44 +0100

    Commit max service feature

--- main.py
+++ main.py
@@ -4,26 +4,32 @@ import os
 
 from nredarwin.webservice import DarwinLdbSession
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
+maxServices = 2 #Maximum number of services to display
 apiKey = os.getenv("apiKey")
 CRS = os.getenv("CRS").upper().strip()
 
+def getServiceData():
 
-Darwin = DarwinLdbSession(wsdl="https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx", api_key=apiKey)
+    Darwin = DarwinLdbSession(wsdl="https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx", api_key=apiKey)
 
-board = Darwin.get_station_board(crs=CRS)
+    board = Darwin.get_station_board(crs=CRS)
 
+    for service in board.train_services:
+        if maxServices <=0:
+            break
+        
+        destination = service.destination_text
+        print (f"Destination : {destination}")
+        dueStatus = service.etd
+        print (f"Departure time : {dueStatus}")
+        operator = service.operator_name
+        print (f"TOC : {operator}")
+        platform = service.platform
+        print (f"Platform : {platform}")
+        print ("------------------------------------------")
 
-
-# for service in board.train_services:
-#     destination = service.destination_text
-#     print (f"Destination : {destination}")
-#     dueStatus = service.etd
-#     print (f"Departure time : {dueStatus}")
-#     operator = service.operator_name
-#     print (f"TOC : {operator}")
-#     platform = service.platform
-#     print (f"Platform : {platform}")
-#     print ("------------------------------------------")
+        maxServices -= 1
