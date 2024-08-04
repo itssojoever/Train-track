2c5f042466b7ed8c24d45fe5d9162cb2ff98bf06
JoeEnterAUsername <144183809+JoeEnterAUsername@users.noreply.github.com>
2024-08-04 21:23:49 +0100

    Add error handling. Refactor code.

--- main.py
+++ main.py
@@ -3,54 +3,76 @@
 #GUI to input station and visualise services. Enable or disable GUI with a switch
 
 import os
+import logging
 import ttkbootstrap as ttk
 
 from nredarwin.webservice import DarwinLdbSession
 from dotenv import load_dotenv
 from datetime import datetime
 
-load_dotenv()
+class Config():
+    def __init__(self, env_file: str=".env"):
+        self.load_config(env_file)
 
-arrival_switch = 2 #1 for arrival time, #2 for departure time
-max_services = 2 #Maximum number of services to display
-platform = 1 #Which platform to check
+        self.arrival_switch = 2 #1 for arrival time, #2 for departure time
+        self.max_services = 2 #Maximum number of services to display
+        self.platform = 1 #Which platform to check
+        self.crs = "MAN" #Remove hardcoding of station later
 
-apiKey = os.getenv("apiKey")
-CRS = os.getenv("CRS").upper().strip()
+        #get apikey
+        self.api_key = os.getenv("api_key")
+        if self.api_key is None:
+            logging.critical("API Key is empty")
+            raise ValueError("API key is empty")
+        
+            
+        
 
-class trainFetcher():
+    def load_config(self, env_file: str=".env"):
+        load_dotenv(env_file)
 
-    def __init__(self):
+
+        
+class TrainFetcher():
+
+    def __init__(self, config: Config):
         self.root = ttk.Window(themename="superhero")
         self.root.position_center()
         self.root.resizable(True, True)
         self.root.geometry("500x500")
         self.root.title("Train track")
         self.root.iconname(None)
+
+        self.config = config
     
     def initMainLoop(self):
         self.root.mainloop()
 
 
-    def service_disruption_check():
+    def service_disruption_check(self):
         pass
 
-    def get_service_data(arrival_switch, max_services):
+    def get_service_data(self):
 
-        Darwin = DarwinLdbSession(wsdl="https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx", api_key=apiKey)
+        try:
 
-        board = Darwin.get_station_board(crs=CRS)
+            Darwin = DarwinLdbSession(wsdl="https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx", api_key=self.config.api_key)
+
+            board = Darwin.get_station_board(crs=self.config.crs)
+        
+        except Exception as e:
+            logging.critical(f"An error occured whilst trying to receive data via API: {e}")
 
         for service in board.train_services:
-            if max_services <=0:
+            if self.config.max_services <=0: #break loop after configured limit of services to be displayed is reached
                 break
             
             destination = service.destination_text
             print (f"Destination : {destination}")
-            if arrival_switch == 2:
+            if self.config.arrival_switch == 2:
                 departure_time = service.std
                 print (f"Departure time : {departure_time}")
-            elif arrival_switch == 1:
+            elif self.config.arrival_switch == 1:
                 arrival_time = service.sta
                 print (f"Arrival time: {arrival_time}")
             operator = service.operator_name
@@ -59,8 +81,9 @@ class trainFetcher():
             print (f"Platform : {platform}")
             print ("------------------------------------------")
 
-            maxServices -= 1
+            self.config.max_services -= 1
 
 if __name__ == '__main__':
-    program = trainFetcher()
+    config = Config()
+    program = TrainFetcher(config)
     program.initMainLoop()
