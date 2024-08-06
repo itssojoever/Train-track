a6c3e738c0eae5a14d06760a1487308f9c370ba9
JoeEnterAUsername <144183809+JoeEnterAUsername@users.noreply.github.com>
2024-08-06 20:04:21 +0100

    Add functionality to display and update textbox with services

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class Config():
         self.load_config(env_file)
 
         self.arrival_switch = 2 #1 for arrival time, #2 for departure time
-        self.max_services = 2 #Maximum number of services to display
+        self.max_services = 100 #Maximum number of services to display
         self.platform = 1 #Which platform to check
         self.crs = ["BHM", "BRV", "UNI"]
 
@@ -49,29 +49,46 @@ class TrainFetcher():
         self.config = config
 
         self.create_gui()
+        self.init_data_fetch()
     
     def init_main_loop(self):
         self.root.mainloop()
 
     def create_gui(self):
-        option_menu_1 = customtkinter.CTkOptionMenu(self.root, values=self.config.crs)
-        option_menu_1.pack(pady=20)
+        self.optionmenu_1 = customtkinter.CTkOptionMenu(self.root, values=self.config.crs, command=self.optionmenu_1_response)
+        self.optionmenu_1.pack(pady=20)
+
+        self.service_display_1 = customtkinter.CTkTextbox(self.root, state="disabled", width=400)
+        self.service_display_1.pack()
+
+    def optionmenu_1_response(self, crs_choice):
+        self.station_chosen = crs_choice
+        self.get_service_data(self.station_chosen)
+        
+    def init_data_fetch(self):
+        initial_crs = self.config.crs[0]
+        self.optionmenu_1_response(initial_crs)
+
 
 
     def service_disruption_check(self):
         pass
 
-    def get_service_data(self):
+    def get_service_data(self, station_chosen):
 
         try:
 
             Darwin = DarwinLdbSession(wsdl="https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx", api_key=self.config.api_key)
 
-            board = Darwin.get_station_board(crs=self.config.crs)
+            board = Darwin.get_station_board(crs=station_chosen)
 
         
         except Exception as e:
             logging.critical(f"An error occured whilst trying to receive data via API: {e}")
+
+        self.service_display_1.configure(state="normal")
+        self.service_display_1.delete("1.0", "end")
+
 
         for service in board.train_services:
             if self.config.max_services <=0: #break loop after configured limit of services to be displayed is reached
@@ -91,7 +108,13 @@ class TrainFetcher():
             print (f"Platform : {platform}")
             print ("------------------------------------------")
 
+            service_output = f"Destination: {destination}\n Departure time: {departure_time}\n\n"
+
+            self.service_display_1.insert("end", service_output)
+
             self.config.max_services -= 1
+        
+        self.service_display_1.configure(state="disabled")
 
 if __name__ == '__main__':
     config = Config()
